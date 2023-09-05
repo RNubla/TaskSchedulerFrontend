dbba39845c072cff9f80f72b8c0a1d1542b25b9b
RNubla <rnubla97@gmail.com>
2023-09-05 12:09:34 -0400

    Table displays tasks

--- components/TaskScheduler.vue
+++ components/TaskScheduler.vue
@@ -1,38 +1,79 @@
 <script setup lang="ts">
-import { FilterMatchMode } from 'primevue/api';
+import { FilterMatchMode, FilterOperator } from 'primevue/api';
 const jobs = ref()
 const loading = ref(true)
 const filters = ref({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS }
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    name: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.CONTAINS }] },
+    path: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.NOT_CONTAINS }] },
+    state: { value: null, matchMode: FilterMatchMode.EQUALS },
+    lastRunTime: { value: null, matchMode: FilterMatchMode.DATE_IS },
+    nextRunTime: { value: null, matchMode: FilterMatchMode.DATE_IS },
+    lastTaskResult: { value: null, matchMode: FilterMatchMode.EQUALS },
 })
 
 onMounted(async () => {
-    await fetch('https://localhost:7202/api/TaskScheduler').then(res => res.json()).then(data => jobs.value = data)
+    const response = await fetch('https://localhost:7202/api/TaskScheduler')
+    response.json().then((data) => jobs.value = transformData(data)).catch((err) => console.error(err))
+    loading.value = false
 })
+
+const transformData = (data) => {
+    return [...(data || [])].map((d) => {
+        // console.log(d)
+        d.lastRunTime = formatDate(d.lastRunTime)
+        d.nextRunTime = formatDate(d.nextRunTime)
+        return d
+    });
+}
+
+const formatDate = (value: string) => {
+    const date = new Date(value)
+    return date.toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    });
+};
+
+const states = ref([
+    'UNKNOWN',
+    'DISABLED',
+    'QUEUED',
+    'READY',
+    'RUNNING'
+])
 </script>
 
 <template>
     <div class="card">
-        <DataTable v-model:filters="filters" :value="jobs" paginator :rows="10"
-            :globalFilterFields="['path', 'name', 'state', 'lastRunTime', 'nextRunTime', 'lastTaskResult', 'enabled']">
+        <DataTable v-model:filters="filters" :value="jobs" stripedRows paginator :rows="10" dataKey="id"
+            filterDisplay="menu" :loading="loading"
+            :globalFilterFields="['path', 'name', 'state', 'lastRunTime', 'nextRunTime', 'lastTaskResult']">
             <template #header>
                 <div class="flex justify-content-end">
                     <span class="p-input-icon-left">
                         <i class="pi pi-search" />
-                        <InputText v-model="filters['global'].value" placeholder="Keyword Search" />
+                        <InputText v-model="filters['global'].value" placeholder="Search the whole table" />
                     </span>
                 </div>
             </template>
             <template #empty> No Task Found</template>
             <template #loading> Loading jobs from Task Scheduler. Please Wait.</template>
             <Column field="path" header="Path" style="min-width: 12rem;" filterField="path">
-                <template #body="{ data }">{{ data.path }}</template>
+                <template #body="{ data }">
+                    {{ data.path }}
+                </template>
                 <template #filter="{ filterModel, filterCallback }">
                     <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
                         placeholder="Search Path"></InputText>
                 </template>
             </Column>
-            <Column field="name" header="Name" style="min-width: 12rem;" filterField="name">
+            <Column field="name" header="Name" sortable style="min-width: 12rem;" filterField="name">
                 <template #body="{ data }">{{ data.name }}</template>
                 <template #filter="{ filterModel, filterCallback }">
                     <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
@@ -42,8 +83,29 @@ onMounted(async () => {
             <Column field="state" header="State" style="min-width: 12rem;" filterField="state">
                 <template #body="{ data }">{{ data.state }}</template>
                 <template #filter="{ filterModel, filterCallback }">
+                    <Dropdown v-model="filterModel.value" type="text" :options="states" @input="filterCallback()"
+                        class="p-column-filter" placeholder="Search By State"></Dropdown>
+                </template>
+            </Column>
+            <Column field="lastRunTime" header="Last Run Time" style="min-width: 12rem;" filterField="lastRunTime">
+                <template #body="{ data }">{{ data.lastRunTime }}</template>
+                <template #filter="{ filterModel, filterCallback }">
                     <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
-                        placeholder="Search By State"></InputText>
+                        placeholder="Last Run Time"></InputText>
+                </template>
+            </Column>
+            <Column field="lastTaskResult" header="Last Task Result" style="min-width: 12rem;" filterField="lastTaskResult">
+                <template #body="{ data }">{{ data.lastTaskResult }}</template>
+                <template #filter="{ filterModel, filterCallback }">
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
+                        placeholder="Last Task Result"></InputText>
+                </template>
+            </Column>
+            <Column field="nextRunTime" header="Next Run Time" style="min-width: 12rem;" filterField="nextRunTime">
+                <template #body="{ data }">{{ data.nextRunTime }}</template>
+                <template #filter="{ filterModel, filterCallback }">
+                    <InputText v-model="filterModel.value" type="text" @input="filterCallback()" class="p-column-filter"
+                        placeholder="Next Run Time"></InputText>
                 </template>
             </Column>
         </DataTable>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
         <TaskScheduler />
+        <!-- <Table /> -->
     </div>
 </template>
