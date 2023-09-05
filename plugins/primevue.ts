eb1a436b49ad25f3b537eb1746d0c7e921a2ad93
RNubla <rnubla97@gmail.com>
2023-09-04 22:10:49 -0400

    Initial Commit

---
+++ plugins/primevue.ts
@@ -0,0 +1,21 @@
+import { defineNuxtPlugin } from "#app";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import DataTable from "primevue/datatable";
+import MultiSelect from "primevue/multiselect";
+import Column from "primevue/column";
+import Dropdown from "primevue/dropdown";
+import TriStateCheckbox from "primevue/tristatecheckbox";
+import InputText from "primevue/inputtext";
+
+export default defineNuxtPlugin((nuxtApp) => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+    nuxtApp.vueApp.component("Button", Button);
+    nuxtApp.vueApp.component("DataTable", DataTable)
+    nuxtApp.vueApp.component("Column", Column)
+    nuxtApp.vueApp.component("MultiSelect", MultiSelect)
+    nuxtApp.vueApp.component("Dropdown", Dropdown)
+    nuxtApp.vueApp.component("TriStateCheckbox", TriStateCheckbox)
+    nuxtApp.vueApp.component("InputText", InputText)
+    //other components that you need
+});
